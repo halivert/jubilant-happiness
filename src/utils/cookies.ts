8f426ee2a1ac2ff30adc792c6a49fa7797d13bc5
Halí V. <halivelazquez@outlook.com>
2024-02-14 22:40:31 -0600

    feat: add domain to cookie

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,6 +7,5 @@ export function getCookie(name: string): string | undefined {
 }
 
 export function deleteCookie(name: string): void {
-  console.info(`Deleting ${name}`)
-  document.cookie = `${name}=; expires=${new Date(0)}`
+  document.cookie = `${name}=; expires=${new Date(0)}; domain=${import.meta.env.VITE_API_DOMAIN}`
 }
